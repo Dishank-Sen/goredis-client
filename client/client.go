edc95c38ed890b9d2b8bff46ca7ba66dfbebd07d
Dishank-Sen <dishanksen05@gmail.com>
2026-04-11 00:19:35 +0530

    added MSET

--- client/client.go
+++ client/client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -98,7 +99,7 @@ func (c *Client) Set(ctx context.Context, key, value string) error {
 	cmdStr := fmt.Sprintf("*3\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",
 		len(cmd), cmd, len(key), key, len(value), value)
 
-	if err := c.writeWithContext(ctx, c.conn, []byte(cmdStr)); err != nil {
+	if err := c.writeWithContext(ctx, []byte(cmdStr)); err != nil {
 		return err
 	}
 
@@ -120,7 +121,7 @@ func (c *Client) Get(ctx context.Context, key string) (string, error) {
 	cmdStr := fmt.Sprintf("*2\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",
 		len(cmd), cmd, len(key), key)
 
-	if err := c.writeWithContext(ctx, c.conn, []byte(cmdStr)); err != nil {
+	if err := c.writeWithContext(ctx, []byte(cmdStr)); err != nil {
 		return "", err
 	}
 
@@ -140,18 +141,67 @@ func (c *Client) Get(ctx context.Context, key string) (string, error) {
 	return "", fmt.Errorf("unexpected response type: %T", resp)
 }
 
-func (c *Client) writeWithContext(ctx context.Context, conn net.Conn, data []byte) error {
+func (c *Client) MSet(ctx context.Context, kvp map[string]string) error{
+	cmd := "MSET"
+	n := len(kvp)*2 + 1
+	var b strings.Builder
+
+	b.WriteByte('*')
+	b.WriteString(strconv.Itoa(n))
+	b.WriteString("\r\n")
+
+	b.WriteByte('$')
+	b.WriteString(strconv.Itoa(len(cmd)))
+	b.WriteString("\r\n")
+
+	b.WriteString(cmd)
+	b.WriteString("\r\n")
+
+	for k, v := range kvp{
+		// write key
+		b.WriteByte('$')
+		b.WriteString(strconv.Itoa(len(k)))
+		b.WriteString("\r\n")
+		b.WriteString(k)
+		b.WriteString("\r\n")
+
+		// write value
+		b.WriteByte('$')
+		b.WriteString(strconv.Itoa(len(v)))
+		b.WriteString("\r\n")
+		b.WriteString(v)
+		b.WriteString("\r\n")
+	}
+
+	if err := c.writeWithContext(ctx, []byte(b.String())); err != nil{
+		return err
+	}
+
+	resp, err := c.readRESP()
+	if err != nil {
+		return err
+	}
+
+	// Expect "OK"
+	if str, ok := resp.(string); ok && str == "OK" {
+		return nil
+	}
+
+	return fmt.Errorf("unexpected response: %v", resp)
+}
+
+func (c *Client) writeWithContext(ctx context.Context, data []byte) error {
 	errCh := make(chan error, 1)
 
 	go func() {
-		_, err := conn.Write(data)
+		_, err := c.conn.Write(data)
 		errCh <- err
 	}()
 
 	select {
 	case <-ctx.Done():
 		// cancel effect → force unblock
-		conn.SetWriteDeadline(time.Now()) // immediate timeout
+		c.conn.SetWriteDeadline(time.Now()) // immediate timeout
 		return ctx.Err()
 
 	case err := <-errCh:
--- client/client_test.go
+++ client/client_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestClient(t *testing.T){
+func TestSet(t *testing.T){
 	cfg := Config{
 		addr: ":5000",
 	}
@@ -22,6 +22,36 @@ func TestClient(t *testing.T){
 
 	curr := time.Now()
 	val, err := client.Get(context.Background(), "name")
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println("duration: ", time.Since(curr))
+	fmt.Printf("value: %s", val)
+}
+
+func TestMSet(t *testing.T){
+	cfg := Config{
+		addr: ":5000",
+	}
+	client := NewClient(cfg)
+	defer client.Close()
+
+	m := make(map[string]string)
+
+	for i := 0; i < 1000; i++ {
+		k := fmt.Sprintf("key%d", i)
+		v := fmt.Sprintf("value %d", i)
+		m[k] = v
+	}
+
+	if err := client.MSet(context.Background(), m); err != nil{
+		panic(err)
+	}
+
+	fmt.Println("keys set")
+
+	curr := time.Now()
+	val, err := client.Get(context.Background(), "key88")
 	if err != nil{
 		panic(err)
 	}
